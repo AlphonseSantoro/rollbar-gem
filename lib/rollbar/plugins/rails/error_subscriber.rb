c2dca56c75fcff4dd35d32a1903a72e9de39ee2b
Kjell-Olaf <kjellolaf@lucalabs.com>
2024-10-18 14:48:22 +0200

    Include person/request scope when enable_rails_error_subscriber is enabled

--- lib/rollbar/plugins/rails/error_subscriber.rb
+++ lib/rollbar/plugins/rails/error_subscriber.rb
@@ -4,9 +4,16 @@ module Rollbar
       # The default `nil` for capture_uncaught means `true`. so check for false.
       return unless handled || Rollbar.configuration.capture_uncaught != false
 
-      extra = context.is_a?(Hash) ? context.deep_dup : {}
-      extra[:custom_data_method_context] = source
-      Rollbar.log(severity, error, extra)
+      controller = context[:controller]
+      scope = {
+        person: controller.rollbar_person_data,
+        request: controller.rollbar_request_data
+      }
+      Rollbar.scoped(scope) do
+        extra = context.is_a?(Hash) ? context.deep_dup : {}
+        extra[:custom_data_method_context] = source
+        Rollbar.log(severity, error, extra)
+      end
     end
   end
 end
